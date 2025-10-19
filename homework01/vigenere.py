951c1cf63f1a2faf005e69c97c489d000da1ace0
vsliozberg <v.sliozberg@gmail.com>
2025-10-19 20:41:48 +0300

    Add Vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,13 @@
+"""
+This module implements the Vigenere cipher for encryption and decryption.
+It provides two main functions:
+1. encrypt_vigenere: Encrypts a given plaintext using the Vigenere cipher.
+2. decrypt_vigenere: Decrypts a given ciphertext using the Vigenere cipher.
+"""
+
+from math import ceil
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +19,16 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword = keyword * ceil(len(plaintext) / len(keyword))
+    for i in range(len(plaintext)):
+        shift = ord(keyword[i]) - ord("A")
+        if "A" <= plaintext[i] <= "Z":  # Заглавные буквы
+            ciphertext += chr((ord(plaintext[i]) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= plaintext[i] <= "z":  # Строчные буквы
+            ciphertext += chr((ord(plaintext[i]) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            ciphertext += plaintext[i]  # Остальные символы не меняем
     return ciphertext
 
 
@@ -24,5 +43,14 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword = keyword * ceil(len(ciphertext) / len(keyword))
+    for i in range(len(ciphertext)):
+        shift = ord(keyword[i]) - ord("A")
+        if "A" <= ciphertext[i] <= "Z":  # Заглавные буквы
+            plaintext += chr((ord(ciphertext[i]) - ord("A") - shift) % 26 + ord("A"))
+        elif "a" <= ciphertext[i] <= "z":  # Строчные буквы
+            plaintext += chr((ord(ciphertext[i]) - ord("a") - shift) % 26 + ord("a"))
+        else:
+            plaintext += ciphertext[i]  # Остальные символы не меняем
     return plaintext
